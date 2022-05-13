ba10453a12a0955550b3286e9ccaf526a65bbe68
Serpedg1o <serpedgio@gmail.com>
2022-05-13 23:06:20 +0300

    modified script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+// const addNewStickerButton = document.getElementById('addNewStickerImg');
+// const addNewSticker = function(){
+//     const newSticker = document.createElement('div');
+//     newSticker.className = 'sticker';
+//     const stickerBox = document.querySelector('.stickerBox');
+//     return stickerBox.append(newSticker);
+//     stickerBox.insertBefore(newSticker, stickerBox.children[stickerBox.lenth]);
+// };
+// addNewStickerButton.onclickt(addNewSticker);
+
+
+document.getElementById('addNewStickerImg').onclick = function(){
+    const newSticker = document.createElement('div');
+    newSticker.className = 'sticker';
+    const stickerBox = document.querySelector('.stickerBox');
+    return stickerBox.append(newSticker);
+    alert("Вы нажали на кнопку");
+  }
