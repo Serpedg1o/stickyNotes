18d35b50a31d47a6964b4d34992a576258bd6a43
Serpedg1o <serpedgio@gmail.com>
2022-05-14 20:40:13 +0300

    try_add_remove_button

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// const addNewStickerButton = document.getElementById('addNewStickerImg');
-// const addNewSticker = function(){
-//     const newSticker = document.createElement('div');
-//     newSticker.className = 'sticker';
-//     const stickerBox = document.querySelector('.stickerBox');
-//     return stickerBox.append(newSticker);
-//     stickerBox.insertBefore(newSticker, stickerBox.children[stickerBox.lenth]);
-// };
-// addNewStickerButton.onclickt(addNewSticker);
-
-
 const createNewSticker = document.getElementById('addNewStickerImg');
 createNewSticker.onclick = () => {
     const newSticker = document.createElement('li');
     newSticker.className = 'sticker';
     const stickerBox = document.querySelector('.stickerBox');
-    return stickerBox.insertBefore(newSticker, stickerBox.lastElementChild);
-    alert('проверка скрипта');
+    stickerBox.insertBefore(newSticker, stickerBox.lastElementChild);
 };
 
-  
+const removeSticker = document.getElementById('closeButton');
+removeSticker.onclick = () => {
+    removeSticker.parentElement.remove();
+};
