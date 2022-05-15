14d45da4f82c6ae3ccf068b11acb5ba358ff945d
Serpedg1o <serpedgio@gmail.com>
2022-05-15 21:26:14 +0300

    remove_sticker_is_work

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const createNewSticker = document.getElementById('addNewStickerImg');
 createNewSticker.onclick = () => {
     const newSticker = document.createElement('li');
     newSticker.className = 'sticker';
+    newSticker.innerHTML = '<input type="image" id="closeButton" src="/img/close.png" alt="Удалить стикер">';
     const stickerBox = document.querySelector('.stickerBox');
     stickerBox.insertBefore(newSticker, stickerBox.lastElementChild);
 };
