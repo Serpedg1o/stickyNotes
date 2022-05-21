235e39db0d33502a3f078a73f083736dfe80cb32
Serpedg1o <serpedgio@gmail.com>
2022-05-21 17:56:17 +0300

    added random color, work remove sticker, work add sticker

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const createNewSticker = document.getElementById('addNewStickerImg');
-createNewSticker.onclick = () => {
-    const newSticker = document.createElement('li');
-    newSticker.className = 'sticker';
-    newSticker.innerHTML = '<input type="image" id="closeButton" src="/img/close.png" alt="Удалить стикер">';
+const availableStickerColors = ["#FED6BC", "#FFFADD", "#DEF7FE", "#E7ECFF", "#C3FBD8", "#FDEED9", "#B5F2EA", "#C6D8FF", ];
+
+const createNewStickerButton = document.getElementById('addNewStickerImg');
+const createNewSticker = () => {
+    const newStickerElement = document.createElement('li');
+    newStickerElement.classList.add('sticker');
+    const randomStickerColor = availableStickerColors[Math.floor(Math.random() * 8)];
+    newStickerElement.style.backgroundColor = `${randomStickerColor}`;
+    const closeButtonElement = document.createElement('input');
+    closeButtonElement.className = 'closeButton';
+    closeButtonElement.type = 'image';
+    closeButtonElement.src = '/img/close.png';
+    closeButtonElement.onclick = () => newStickerElement.remove(); 
+    newStickerElement.append(closeButtonElement);   
     const stickerBox = document.querySelector('.stickerBox');
-    stickerBox.insertBefore(newSticker, stickerBox.lastElementChild);
+    stickerBox.insertBefore(newStickerElement, stickerBox.lastElementChild);
 };
 
-const removeSticker = document.getElementById('closeButton');
-removeSticker.onclick = () => {
-    removeSticker.parentElement.remove();
-};
+createNewStickerButton.onclick = createNewSticker;
